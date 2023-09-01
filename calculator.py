f0306dc01894e8a66f52667e522f296c0de11c01
Laura Díez <lauradiezdeveloper@gmail.com>
2023-09-01 13:50:20 +0200

    Added test

--- calculator.py
+++ calculator.py
@@ -4,7 +4,7 @@ numero1_str, numero2_str = input("Añade los dos números sobre los que quieres 
 x = int(numero1_str)
 y = int(numero2_str)
 
-def Calculadora():
+def calculadora(operacion, x, y):
     if operacion == "sumar":
         print(x+y)
     elif operacion == "restar":
@@ -12,8 +12,13 @@ def Calculadora():
     elif operacion == "multiplicar":
         print(x*y)
     elif operacion == "dividir":
-        print(x//y)
+        print(x/y)
     elif operacion == "potencia":
         print(x**y)
 
-Calculadora()
+#Test
+calculadora("sumar", x, y)
+calculadora("restar", x, y)
+calculadora("multiplicar", x, y)
+calculadora("dividir", x, y)
+calculadora("potencia", x, y)
