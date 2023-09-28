deed1f32bc7983bcb434392a35315b3f6995c749
Laura Díez <lauradiezdeveloper@gmail.com>
2023-09-28 12:11:40 +0200

    Added dictioraies

--- playlist.py
+++ playlist.py
@@ -9,12 +9,14 @@ print("Ejemplo: 'rock', y obtendrás un playlist de canciones o artistas que inc
 word_to_search= input()
 url = "https://deezerdevs-deezer.p.rapidapi.com/search" 
 querystring = {"q":word_to_search}
-
 headers = {
 "X-RapidAPI-Key": "3ed98f551fmshe4e6260228c4eadp13db17jsn8c893e1194da", 
 "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com"
 }
 response = requests.request("GET", url, headers=headers, params=querystring).json() 
+
+my_list = []
+
 for element in response['data']:
-    my_list = [element['title'], element['artist']['name']]
-    print(tuple(my_list))
+    my_list.append((element['title'], element['artist']['name']))
+    print(dict(my_list))
